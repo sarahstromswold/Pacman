4bc31f0b9bd9e786bd0b658830e36f9f31d6372f
Maggie Bailey <mbailey26@amherst.edu>
2023-04-13 21:50:06 -0400

    wassup this is a test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-public class Main {
+git@github.com:sarahstromswold/Pacman.gitpublic class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("bye");
+	//hello Sarah this is Mags
     }
 }
