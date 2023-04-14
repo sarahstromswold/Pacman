3f90a5869df10eaad936b5eb354bcbc22b667f06
Sarah Stromswold <sarahstromswold@gmail.com>
2023-04-14 18:36:56 -0400

    moving pacman

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
-class Main extends JPanel {
+public class Main extends JPanel implements KeyListener{
     public static final int WIDTH = 900;
     public static final int HEIGHT = 600;
     public static final int FPS = 60;
@@ -15,10 +15,12 @@ class Main extends JPanel {
 
     public Main() {
 	world = new World (WIDTH, HEIGHT);
+    addKeyListener(this);
+    Thread mainThread = new Thread(new Runner());
+    mainThread.start();
 	this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-    world.update(FPS);
-	repaint();
     }
+
     public static void main(String[] args) {
 	JFrame frame = new JFrame("PacMan");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,6 +38,49 @@ class Main extends JPanel {
 	world.drawWorld(g);
     }
     //need menu method
+
+    public void keyPressed(KeyEvent e) {
+        char c = e.getKeyChar();
+        if (c == 'w') {
+            world.pacman.velocityY = -100;
+            world.pacman.velocityX = 0;
+        }
+        if (c == 'a') {
+            world.pacman.velocityX = -100;
+            world.pacman.velocityY = 0;
+        }
+        if (c == 's') {
+            world.pacman.velocityY = 100;
+            world.pacman.velocityX = 0;
+        }
+        if (c == 'd') {
+            world.pacman.velocityX = 100;
+            world.pacman.velocityY = 0;
+        }
+    }
+    class Runner implements Runnable {
+        public void run() {
+            while (true) {
+                world.update(1.0 / (double) FPS);
+                repaint();
+                try {
+                    Thread.sleep(1000 / FPS);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();
+    }
 }
 
 class World {
@@ -56,13 +101,14 @@ class World {
     public void drawWorld(Graphics g) {
 	g.setColor(Color.WHITE);
 	//g.drawString("Points: " + numPoints);
-
 	pacman.draw(g);
 	//draw fire guys
 	//draw points
 	//draw powerups
     }
+
     public void update(double time) {
     pacman.update(time);
     }
+
 }
--- src/PacMan.java
+++ src/PacMan.java
@@ -3,60 +3,37 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class PacMan implements KeyListener {
+public class PacMan {
     int radius;
     double initialPositionX;
     double initialPositionY;
     double positionX;
     double positionY;
-    double velocity;
+    double velocityX;
+    double velocityY;
     int numLives;
-    Color color;
     public PacMan(World w) {
         initialPositionX = 50;
         initialPositionY = w.height - 50;
         positionX = 50;
         positionY = 500;
-        velocity = 40;
+        velocityX = 0;
+        velocityY = 0;
         radius = 25;
-        color = Color.YELLOW;
 
     }
 
     public void update(double time) {
-        positionX = positionX + (velocity * time);
-        positionY = positionY + (velocity * time);
+        positionX = positionX + (velocityX * time);
+        positionY = positionY + (velocityY * time);
     }
 
     public void draw(Graphics g) {
         //Color color = g.getColor();
         g.setColor(Color.YELLOW);
         g.fillArc((int)positionX,(int)positionY,50,50,25,180);
+        //velocity x neg, point
         g.fillArc((int)positionX,(int)positionY,50,50, -25,-180);
-        g.setColor(color);
     }
-    public void keyPressed(KeyEvent e) {
-        char c = e.getKeyChar();
-        if (c == 'w') {
-            velocity = -1 * velocity;
-        }
-        if (c == 'a') {
-            velocity = -1 * velocity;
-        }
-        if (c == 's') {
-            this.velocity = velocity;
-        }
-        if (c == 'd') {
-            this.velocity = velocity;
-        }
-    }
-
-    public void keyReleased(KeyEvent e) {
-    }
-
-    public void keyTyped(KeyEvent e) {
-    }
-
-    public void addNotify() {}
 
 }
