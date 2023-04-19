05a3e077939bbf82af9df81b28de7ed017f7a712
maggiebailey <mbailey26@amherst.edu>
2023-04-19 16:19:26 -0400

    fixed cell in middle

--- src/Maze.java
+++ src/Maze.java
@@ -24,7 +24,7 @@ public class Maze {
 	    for (int j = 0; j < 5; j++) {
 		int t = rand.nextInt(4);
 		int r = rand.nextInt(4);
-		if (i == 2 && j == 1) {
+		if (i == 1 && j == 2) {
 		    Tile c = new Tile(4, 0);
 		    addTile(c, i, j);
 		}
